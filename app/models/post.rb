2abae53bb1f3d4d7945594c21b5474a83f31c690
kohkoh22 <kohnosuke@gmail.com>
2020-08-23 23:09:34 +0900

    Updated show-next-previous-function
    詳細ページの次へと前へ

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,11 @@ class Post < ApplicationRecord
   mount_uploader :image, ImageUploader
   belongs_to :user
   has_many :comments
+  def previous
+    user.posts.order('created_at desc, id desc').where('created_at <= ? and id < ?', created_at, id).first
+  end
+
+  def next
+    user.posts.order('created_at desc, id desc').where('created_at >= ? and id > ?', created_at, id).reverse.first
+  end
 end
