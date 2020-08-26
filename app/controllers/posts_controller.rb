ad38b4e9d3d02c861e1c01216e07954073a7e888
kohkoh22 <kohnosuke@gmail.com>
2020-08-26 12:34:19 +0900

    Updated guest-login-function
    全体のビューの調整
    ゲストログインはまだ
    Twitterでシェア

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -35,6 +35,8 @@ end
 
 def show
   @post = Post.find(params[:id])
+  @posts = Post.order(impressions_count: 'DESC')
+  @postss = Post.all.sort {|a,b| b.liked_users.count <=> a.liked_users.count}
   impressionist(@post, nil, unique: [:session_hash])
   @comment = Comment.new
   @comments = @post.comments.includes(:user)
