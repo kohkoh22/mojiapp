44ad3caa0e7a7b97d16503fc3abe727bcad2027a
kohkoh22 <kohnosuke@gmail.com>
2020-09-01 19:56:06 +0900

    updated refactoring
    コードとレイアウトの整理

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,30 +1,26 @@
 class UsersController < ApplicationController
-  before_action :authenticate_user!, only: [:show, :edit, :update]
+  before_action :authenticate_user!, only: [ :edit, :update]
+  before_action :set_user, only: [:show, :edit, :update, :following, :followers]
   def index
     @users = User.all.sort {|a,b| b.followed.count <=> a.followed.count}
   end
   
   def show
-    @post = current_user.posts
-    @user = User.find(params[:id])
     @tags = ActsAsTaggableOn::Tag.all
-    # impressionist(@user, nil, unique: [:session_hash])
   end
 
   def edit
-    @user = User.find(params[:id])
     unless @user == current_user
        redirect_to user_path(@user)
-     end
+    end
   end
   
   def update
-    @user = User.find(params[:id])
     if @user.update(user_params)
       redirect_to user_path(current_user)
     else
      redirect_to edit_user_path(current_user)
-     end
+    end
   end
 
   def search
@@ -32,20 +28,22 @@ class UsersController < ApplicationController
   end
 
   def following
-    @user  = User.find(params[:id])
     @users = @user.follower
     render 'following'
-end
+  end
 
-def followers
-  @user  = User.find(params[:id])
-  @users = @user.followed
-  render 'followed'
-end
+  def followers
+    @users = @user.followed
+    render 'followed'
+  end
 
 
   private
   def user_params
     params.fetch(:user, {}).permit(:nickname, :image, :profile)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
   end
 end
